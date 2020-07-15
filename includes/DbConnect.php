4ca46fe5dae614cbede6e9b898b1a8e826f881d5
lalomiguel17 <67929893+lalomiguel17@users.noreply.github.com>
2020-07-15 18:59:49 -0500

    cambio2

--- includes/DbConnect.php
+++ includes/DbConnect.php
@@ -7,7 +7,7 @@
             $dbhost=getenv('CONEXION_BD_IP_SERVIDOR');
             $dbuser=getenv('CONEXION_BD_USUARIO');
             $dbpass=getenv('CONEXION_BD_PASSWORD');
-            $dbname="usuarios";
+            $dbname="usuario_av";
             $this->con =new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass,array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES  \'UTF8\''));
             $this->con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
              if(mysqli_connect_errno()){
