c9f9c5a1e550e7be1c370a9dd88c7e7db209bb7b
lalomiguel17 <67929893+lalomiguel17@users.noreply.github.com>
2020-07-20 18:47:59 -0500

    Aqui va la funcion de pacientes

---
+++ public/funciones/pacientes.php
@@ -0,0 +1,34 @@
+<?php
+include 'cn.php';
+
+//$id=$_POST["idpaciente"];
+$Nombre=$_POST["Nombre"];
+$Edad=$_POST["Edad"];
+$Sexo=$_POST["Sexo"];
+$Direccion=$_POST["Direccion"];
+
+
+$insertar="INSERT INTO Pacientes(Nombre,Edad,Sexo,Direccion)VALUES('$nombre',"
+        . "'$edad','$sexo','$direccion')";
+
+
+$verificar_usuario= mysqli_query($conexion,"SELECT*FROM Pacientes WHERE Pacientes='$Pacientes'");
+
+if(mysqli_num_rows($verificar_usuario)>0){
+    
+   echo'El paciente ya esta registrado'; 
+   exit;
+}
+
+
+$resultado = mysqli_query($conexion, $insertar);
+if(!$resultado){
+    
+  echo 'error al registrarse';  
+}else{
+    
+    echo 'usuario registrado';    
+    
+}
+
+mysqli_close($conexion);
