c4762ec0e5eeba2766a0900ab63859d740e82ba8
lalomiguel17 <67929893+lalomiguel17@users.noreply.github.com>
2020-07-20 19:50:12 -0500

    dsadw4adwe

--- public/funciones/paciente.php
+++ public/funciones/paciente.php
@@ -14,21 +14,21 @@ class Login{
 
 
 function AccessPaciente($request){
-  $paciente;
-  $response;
-  $pacientes=json_decode($request->getBody());
-  $sql="INSERT INTO Pacientes(idPaciente,Nombre,Edad,Sexo,Direccion) VALUES(:idPaciente,:Nombre,:Edad,:Sexo,:Direccion)";    
-  try{            
-      $statement=$this->conexion->prepare($sql);
-      $statement->bindParam("idPaciente",$paciente->Nombre);
-      $statement->bindParam("Nombre",$paciente->Nombre);
-      $statement->bindParam("Edad",$paciente->edad);
-      $statement->bindParam("Sexo",$paciente->sexo;
-      $statement->bindParam("Direccion",$paciente->direccion);
-      $statement->execute();
-      $response->mensaje="El paciente se inserto correctamente";
-  }catch(Exception $e){
-      $response=$e;
-  }
-  return json_encode($response);
-}
+    $paciente;
+    $response;
+    $pacientes=json_decode($request->getBody());
+    $sql="INSERT INTO Pacientes(idPaciente,Nombre,Edad,Sexo,Direccion) VALUES(:idPaciente,:Nombre,:Edad,:Sexo,:Direccion)";    
+    try{            
+        $statement=$this->conexion->prepare($sql);
+        $statement->bindParam("idPaciente",$paciente->idPaciente);
+        $statement->bindParam("Nombre",$paciente->Nombre);
+        $statement->bindParam("Edad",$paciente->Edad);
+        $statement->bindParam("Sexo",$paciente->Sexo;
+        $statement->bindParam("Direccion",$paciente->Direccion);
+        $statement->execute();
+        $response->mensaje="El paciente se inserto correctamente";
+    }catch(Exception $e){
+        $response=$e;
+    }
+    return json_encode($response);
+     
