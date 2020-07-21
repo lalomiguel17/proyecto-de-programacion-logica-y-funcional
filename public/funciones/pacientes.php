c0ac7a27b3f8e358454d45eb0b76945a12f63a82
lalomiguel17 <67929893+lalomiguel17@users.noreply.github.com>
2020-07-20 19:10:08 -0500

    cambio

--- public/funciones/pacientes.php
+++ public/funciones/pacientes.php
@@ -9,13 +9,14 @@ function setpaciente($request){
   $pacientes;
   $response;
   $paciente=json_decode($request->getBody());
-  $sql="INSERT INTO paciente(nombre,edad,sexo,direccion) VALUES(:nombre,:edad,:sexo,:direccion)";    
+  $sql="INSERT INTO Pacientes(idPaciente,Nombre,Edad,Sexo,Direccion) VALUES(:idPaciente,:Nombre,:Edad,:Sexo,:Direccion)";    
   try{            
       $statement=$this->conexion->prepare($sql);
-      $statement->bindParam("nombre",$paciente->nombre);
-      $statement->bindParam("edad",$paciente->edad);
-      $statement->bindParam("sexo",$paciente->sexo;
-      $statement->bindParam("direccion",$paciente->direccion);
+      $statement->bindParam("idPaciente",$paciente->Nombre);
+      $statement->bindParam("Nombre",$paciente->Nombre);
+      $statement->bindParam("Edad",$paciente->edad);
+      $statement->bindParam("Sexo",$paciente->sexo;
+      $statement->bindParam("Direccion",$paciente->direccion);
       $statement->execute();
       $response->mensaje="El paciente se inserto correctamente";
   }catch(Exception $e){
